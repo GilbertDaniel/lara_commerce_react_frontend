d995335e77f7f70f4b5c805e6267406e7a551f9b
GilbertDaniel <daniel.gilbert247@gmail.com>
2022-05-30 14:30:47 +0630

    Creating Necessary Components for Home

---
+++ src/components/common/FooterDestop.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export class FooterDestop extends Component {
+  render() {
+    return (
+      <div>FooterDestop</div>
+    )
+  }
+}
+
+export default FooterDestop
