46eaf81dcbf19c23fbd1d7a6d912079d07e3abd7
GilbertDaniel <daniel.gilbert247@gmail.com>
2022-05-30 15:46:05 +0630

    Implementing New Arrival Component

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,11 +1,13 @@
 import React, { Component, Fragment } from 'react'
 import Categories from '../components/home/Categories';
+import NewArrival from '../components/home/NewArrival';
 import { FeaturedProducts } from './../components/home/FeaturedProducts';
 
 export class HomePage extends Component {
   render() {
     return (
       <Fragment>
+          <NewArrival/>
           <FeaturedProducts/>
           <Categories/>
       </Fragment>
