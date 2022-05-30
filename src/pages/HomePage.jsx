6288a02dd1c646b4caaf68386d60953e07d78405
GilbertDaniel <daniel.gilbert247@gmail.com>
2022-05-30 16:37:40 +0630

    Implementing Menu for Desktop

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,4 +1,5 @@
 import React, { Component, Fragment } from 'react'
+import NavMenuDesktop from '../components/common/NavMenuDesktop';
 import Categories from '../components/home/Categories';
 import HomeTop from '../components/home/HomeTop';
 import NewArrival from '../components/home/NewArrival';
@@ -8,6 +9,7 @@ export class HomePage extends Component {
   render() {
     return (
       <Fragment>
+          <NavMenuDesktop/>
           <HomeTop/>
           <NewArrival/>
           <FeaturedProducts/>
