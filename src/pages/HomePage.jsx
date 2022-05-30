4bad0af5881d85d487a9a54a1f992b8011d25e1e
GilbertDaniel <daniel.gilbert247@gmail.com>
2022-05-30 17:15:18 +0630

    Implementing Mobile Menu

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,7 +1,9 @@
 import React, { Component, Fragment } from 'react'
 import NavMenuDesktop from '../components/common/NavMenuDesktop';
+import NavMenuMobile from '../components/common/NavMenuMobile';
 import Categories from '../components/home/Categories';
 import HomeTop from '../components/home/HomeTop';
+import HomeTopMobile from '../components/home/HomeTopMobile';
 import NewArrival from '../components/home/NewArrival';
 import { FeaturedProducts } from './../components/home/FeaturedProducts';
 
@@ -9,11 +11,18 @@ export class HomePage extends Component {
   render() {
     return (
       <Fragment>
-          <NavMenuDesktop/>
-          <HomeTop/>
-          <NewArrival/>
-          <FeaturedProducts/>
-          <Categories/>
+        <div className="Desktop">
+          <NavMenuDesktop />
+          <HomeTop />
+        </div>
+
+        <div className="Mobile">
+          <NavMenuMobile />
+          <HomeTopMobile />
+        </div>
+        <NewArrival />
+        <FeaturedProducts />
+        <Categories />
       </Fragment>
     )
   }
