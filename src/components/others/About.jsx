ce6521e0ee8b0917ec49a260130cb0f35b6d4dc0
GilbertDaniel <daniel.gilbert247@gmail.com>
2022-05-31 22:57:56 +0630

    Cosume Site Info API for Pages

--- src/components/others/About.jsx
+++ src/components/others/About.jsx
@@ -1,7 +1,28 @@
 import React, { Component, Fragment } from 'react'
 import { Container, Row, Col, Form, Button } from 'react-bootstrap'
+import AppURL from '../../api/AppURL';
+import axios from 'axios';
 
 class About extends Component {
+    constructor() {
+        super();
+        this.state = {
+            about: ""
+        }
+    }
+
+    componentDidMount() {
+        axios.get(AppURL.AllSiteInfo).then(response => {
+            let StatusCode = response.status;
+            if (StatusCode == 200) {
+                let JsonData = (response.data)[0]['about'];
+                this.setState({ about: JsonData });
+            }
+
+        }).catch(error => {
+
+        });
+    }
     render() {
         return (
             <Fragment>
@@ -10,15 +31,7 @@ class About extends Component {
                         <Col className="shadow-sm bg-white mt-2" md={12} lg={12} sm={12} xs={12}>
                             <h4 className="section-title-login">About Us Page </h4>
                             <p className="section-title-contact">
-                                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin dictum egestas arcu. Duis posuere suscipit massa, sed accumsan odio efficitur nec. Integer in felis diam. Cras facilisis ex sit amet dictum venenatis. Aenean magna diam, gravida et ex vitae, consectetur placerat metus. Quisque consectetur metus eu est malesuada, maximus ornare erat finibus. Aenean nec finibus lacus, pretium consequat sem.
-
-                                Aliquam erat volutpat. Aenean diam turpis, bibendum a sapien vel, posuere vestibulum ex. Pellentesque porta sed augue vel laoreet. Etiam scelerisque odio eu bibendum condimentum. Sed non odio velit. Nam ullamcorper sapien dignissim efficitur vulputate. In mi tortor, lacinia sed congue nec, dictum vel erat. Nunc ut facilisis velit. Praesent in mollis quam, non ullamcorper quam. Aliquam vel magna velit. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Donec et ex et mauris rutrum eleifend id pellentesque dolor. Nullam rutrum, risus sollicitudin vehicula bibendum, orci sapien lobortis neque, quis posuere risus metus ut erat.
-
-                                Aenean et sem ac diam tristique pretium id et ipsum. Nullam vitae elit gravida, suscipit ex non, ornare leo. Cras a aliquet est, non aliquet mi. Vestibulum felis dui, commodo quis nunc eget, lobortis sagittis arcu. Sed lacinia aliquam est, et aliquet nisi malesuada id. Pellentesque mattis erat vel velit gravida, sed ullamcorper orci tempor. Cras auctor dictum ipsum, tincidunt facilisis ante euismod sed. Sed dictum erat et mi tempus scelerisque et non nibh. Fusce condimentum lobortis tempor. Proin nec ipsum ante. Morbi nulla quam, elementum id gravida at, interdum suscipit ligula.
-
-                                Etiam fringilla purus lacus, quis faucibus eros facilisis nec. Suspendisse sapien diam, fringilla eu aliquet ut, malesuada in odio. Vivamus euismod dolor sed nisi ultricies, id iaculis nunc lobortis. Morbi faucibus ligula pellentesque, fermentum sapien eu, aliquam augue. Fusce tincidunt sagittis quam at pretium. Nullam eu quam vitae velit aliquam auctor vel eget neque. Vestibulum in commodo enim, ut suscipit est. Ut interdum non ligula quis venenatis. Duis ultrices mauris leo. In faucibus pretium urna, tempus egestas turpis. Nunc quis tellus vitae magna convallis ultricies id nec enim.
-
-                                Fusce et condimentum orci. Praesent leo mauris, faucibus quis mi ac, elementum mollis tortor. In viverra feugiat mollis. Nunc sed odio lorem. Curabitur facilisis felis a dui vestibulum, id tincidunt ex ullamcorper. Integer placerat vel nunc quis pulvinar. Aliquam ac erat nunc. Maecenas imperdiet elit volutpat, lobortis dui at, accumsan risus. Fusce lobortis vehicula odio quis rhoncus. Donec volutpat, ex vel dictum laoreet, purus eros blandit est, at vulputate odio nisl vel dolor. Nam ut nulla magna. Morbi ac pulvinar dolor.
+                            {this.state.about}
                             </p>
                         </Col>
                     </Row>
