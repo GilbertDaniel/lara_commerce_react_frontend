480c47d31407d0fe6076f8b66d5a308beaf2a94c
GilbertDaniel <daniel.gilbert247@gmail.com>
2022-05-31 09:44:01 +0630

    Implementing User Login Register Component

--- src/route/AppRoute.js
+++ src/route/AppRoute.js
@@ -1,14 +1,16 @@
 import React, { Component, Fragment } from 'react'
-import { Route, Switch } from 'react-router-dom'
-import { HomePage } from './../pages/HomePage';
+import { Routes, Route } from 'react-router-dom'
+import HomePage from './../pages/HomePage';
+import UserLoginPage from './../pages/UserLoginPage';
 
 export class AppRoute extends Component {
   render() {
     return (
       <Fragment>
-          <Switch>
-             <Route exact to="/" component={HomePage}/> 
-          </Switch>
+          <Routes>
+            <Route path='/' element={<HomePage />} />
+            <Route path='/login' element={<UserLoginPage />} />
+        </Routes>
       </Fragment>
     )
   }
